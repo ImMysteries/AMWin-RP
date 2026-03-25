583f9bc43a703c78236232853bccff402f1149c3
mysteries. <125200302+ImMysteries@users.noreply.github.com>
2026-03-25 13:28:48 -0400

    privatized

--- AMWin-RichPresence/Constants.cs
+++ AMWin-RichPresence/Constants.cs
@@ -3,42 +3,50 @@ using System.Diagnostics;
 using System.IO;
 using System.Net.Http;
 
-namespace AMWin_RichPresence {
-    public static class Constants {
-        public static string ProgramVersionBase {
-            get {
-                try {
+namespace AMWin_RichPresence
+{
+    public static class Constants
+    {
+        public static string ProgramVersionBase
+        {
+            get
+            {
+                try
+                {
                     var exePath = Process.GetCurrentProcess().MainModule?.FileName;
-                    if (exePath == null) {
+                    if (exePath == null)
+                    {
                         return "";
                     }
                     FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(exePath);
                     return $"v{fvi.FileVersion}";
-                } catch (Exception ex) {
+                }
+                catch (Exception ex)
+                {
                     new Logger().Log($"Error getting version string: {ex}");
                     return "";
                 }
             }
-        } 
+        }
 #if RELEASE
         public static string  ProgramVersion = ProgramVersionBase;
 #else
-        public static string  ProgramVersion = $"{ProgramVersionBase}-dev";
+        public static string ProgramVersion = $"{ProgramVersionBase}-dev";
 #endif                        
-        public static int    MaxLogFiles                    = 10; // files
-        public static int    RefreshPeriod                  = 5; // seconds
-        public static int    NumFailedSearchesBeforeAbandon = 5; // attempts
-        public static string ApplicationStylisedName        = "AMWin-RichPresence";
-        public static string DiscordClientID                = "1066220978406953012";
-        public static string DiscordAppleMusicImageKey      = "applemusic1024x";
-        public static string DiscordAppleMusicPlayImageKey  = "applemusicplay1024x";
+        public static int MaxLogFiles = 10; // files
+        public static int RefreshPeriod = 5; // seconds
+        public static int NumFailedSearchesBeforeAbandon = 5; // attempts
+        public static string ApplicationStylisedName = "AMWin-RichPresence (Private Mode)";
+        public static string DiscordClientID = "1066220978406953012";
+        public static string DiscordAppleMusicImageKey = "applemusic1024x";
+        public static string DiscordAppleMusicPlayImageKey = "applemusicplay1024x";
         public static string DiscordAppleMusicPauseImageKey = "applemusicpause1024x";
-        public static string LastFMCredentialTargetName     = "Last FM Password";
-        public static int    LastFMTimeBeforeScrobbling     = 20; // seconds
-        public static string GithubReleasesApiUrl           = "https://api.github.com/repos/PKBeam/AMWin-RP/releases/latest";
-        public static string GithubReleasesUrl              = "https://github.com/PKBeam/AMWin-RP/releases";
-        public static string GithubRepoUrl                  = "https://github.com/PKBeam/AMWin-RP/";
-        public static string DefaultAppleMusicRegion        = "US";
+        public static string LastFMCredentialTargetName = "Last FM Password";
+        public static int LastFMTimeBeforeScrobbling = 20; // seconds
+        public static string GithubReleasesApiUrl = "https://api.github.com/repos/ImMysteries/AMWin-RP/releases/latest";
+        public static string GithubReleasesUrl = "https://github.com/ImMysteries/AMWin-RP/releases";
+        public static string GithubRepoUrl = "https://github.com/ImMysteries/AMWin-RP";
+        public static string DefaultAppleMusicRegion = "US";
         public static string WindowsStartupFolder => Environment.GetFolderPath(Environment.SpecialFolder.Startup);
         public static string WindowsAppDataFolder => Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
         public static string AppDataFolder => Path.Combine(WindowsAppDataFolder, ApplicationStylisedName);
